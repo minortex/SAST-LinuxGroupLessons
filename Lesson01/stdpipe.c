94d64618c6e18942ea90799231aa5b02b1fc1975
f3rmata <fermataa@gmx.com>
2024-10-27 16:30:13 +0800

    added c examples.

--- Lesson01/stdpipe.c
+++ Lesson01/stdpipe.c
@@ -1,9 +1,22 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
 
-  write();
+  char string_stdout[] = "Hello World from STDOUT\n";
+  char string_stderr[] = "Hello World from STDERR\n";
+
+  write(1, string_stdout, sizeof(string_stdout));
+  write(2, string_stderr, sizeof(string_stderr));
+
+  int ret = close(1);
+  if (ret == -1) {
+    perror("close error!");
+    return 1;
+  }
+
+  write(1, string_stdout, sizeof(string_stdout));
+  write(2, string_stderr, sizeof(string_stderr));
 
   return 0;
 }
